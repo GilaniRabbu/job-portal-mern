aed093d459832475e7dce012b2b6cfa4110d1aa9
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-08-31 19:26:59 +0600

    updated jobs model

--- src/app/modules/Jobs/jobs.model.ts
+++ src/app/modules/Jobs/jobs.model.ts
@@ -3,15 +3,15 @@ import { Schema, model } from 'mongoose';
 const JobSchema = new Schema(
   {
     title: { type: String, required: true },
-    projectType: { type: String, required: true }, // e.g. Fixed Price Project
-    priceRange: { type: String, required: true }, // e.g. $1,200-$1,400
+    projectType: { type: String, required: true },
+    priceRange: { type: String, required: true },
     description: { type: String, required: true },
 
-    tags: { type: [String], default: [] }, // e.g. ["Remote", "Senior level"]
-    categories: { type: [String], default: [] }, // e.g. ["App Design", "UI/UX"]
+    tags: { type: [String], default: [] },
+    categories: { type: [String], default: [] },
 
-    postedBy: { type: String, required: true }, // e.g. Eamman Olio (or could be a ref to User later)
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Owner user
+    postedBy: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     timestamps: true,
