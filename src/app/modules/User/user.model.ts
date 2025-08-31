6f10386b18f10736d7ed98cabbace9c53cc50d2a
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-08-31 14:17:41 +0600

    updated user model

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -5,6 +5,18 @@ const UserSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['SUPER_ADMIN', 'ADMIN', 'USER'],
+      default: 'USER',
+    },
+    userStatus: {
+      type: String,
+      enum: ['ACTIVE', 'INACTIVE', 'BLOCKED', 'PENDING', 'DELETED'],
+      default: 'ACTIVE',
+    },
+    isDeleted: { type: Boolean },
+    deletedAt: { type: Date },
   },
   {
     timestamps: true,
@@ -12,6 +24,9 @@ const UserSchema = new Schema(
     toObject: { virtuals: true },
   }
 );
+
+UserSchema.index({ userStatus: 1 });
+UserSchema.index({ role: 1 });
 
 const User = model('User', UserSchema);
 
